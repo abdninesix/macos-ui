67b2d7ab25f9fe5ef72f0c5f684b7d03eea275a4
abdninesix <mohabd1995@gmail.com>
2025-12-05 20:48:00 +0500

    Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import { navIcons, navLinks } from "../constants/data"
 
 const Navbar = () => {
     return (
         <nav>
             <div>
-                <img src='/images/logo.svg' alt='logo'/>
+                <img src='/images/logo.svg' alt='logo' />
+                <p className='font-bold'>macFolio</p>
+                <ul>
+                    {navLinks.map((link) => (
+                        <li key={link.id}>
+                            <p>{link.name}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                    {navIcons.map((icon) => (
+                        <li key={icon.id}>
+                            <img src={icon.img} alt={`icon-${icon.id}`} className="icon-hover" />
+                        </li>
+                    ))}
+                </ul>
+                <time>
+                    {new Intl.DateTimeFormat('en-US', { weekday: 'short', hour: '2-digit', minute: '2-digit', hour12: true, year: 'numeric', month: 'short', day: '2-digit' })
+                        .format(new Date())}
+                </time>
             </div>
         </nav>
     )
