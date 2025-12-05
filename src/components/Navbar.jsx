365191e07b19eff81da238c470365c8a0be6cab8
abdninesix <mohabd1995@gmail.com>
2025-12-05 21:18:18 +0500

    Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,12 +19,12 @@ const Navbar = () => {
                 <ul>
                     {navIcons.map((icon) => (
                         <li key={icon.id}>
-                            <img src={icon.img} alt={`icon-${icon.id}`} className="icon-hover" />
+                            <img src={icon.img} alt={`icon-${icon.id}`} className="hover:opacity-80 cursor-pointer" />
                         </li>
                     ))}
                 </ul>
                 <time>
-                    {new Intl.DateTimeFormat('en-US', { weekday: 'short', hour: '2-digit', minute: '2-digit', hour12: true, year: 'numeric', month: 'short', day: '2-digit' })
+                    {new Intl.DateTimeFormat('en-US', { weekday: 'short', hour: '2-digit', minute: '2-digit', hour12: true, year: 'numeric', month: '2-digit', day: '2-digit' })
                         .format(new Date())}
                 </time>
             </div>
