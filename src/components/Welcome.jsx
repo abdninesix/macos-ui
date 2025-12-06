32420e61b31d2bf9991b4a3fb2fbe9d728b9d5d3
abdninesix <mohabd1995@gmail.com>
2025-12-06 18:50:24 +0500

    Updated

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,10 +5,17 @@ const Welcome = () => {
 
     const titleRef = useRef(null)
     const subtitleRef = useRef(null)
-    
-  return (
-    <div>Welcome</div>
-  )
+
+    return (
+        <section id='welcome'>
+            <p ref={subtitleRef}>Hi this is my web based macOS portfolio.</p>
+            <h1 ref={titleRef} className='mt-6'>macFolio</h1>
+
+            <div className='small-screen'>
+                <p>This porfolio is only responsive on desktop/tablet.</p>
+            </div>
+        </section>
+    )
 }
 
 export default Welcome
