5760f71ae3e14020e5e79dfd5288311126ca215f
abdninesix <mohabd1995@gmail.com>
2025-12-06 18:30:54 +0500

    Updated

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useRef } from 'react'
+
+const Welcome = () => {
+
+    const titleRef = useRef(null)
+    const subtitleRef = useRef(null)
+    
+  return (
+    <div>Welcome</div>
+  )
+}
+
+export default Welcome
