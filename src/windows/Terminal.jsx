e5d55fae2c9137fc13687f62a9329e2531c6bf0b
abdninesix <mohabd1995@gmail.com>
2025-12-08 22:48:30 +0500

    Updated

--- src/windows/Terminal.jsx
+++ src/windows/Terminal.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import WindowWrapper from '../hoc/WindowWrapper';
+import { techStack } from '../constants/data';
+import { Check } from 'lucide-react';
 
 const Terminal = () => {
     return (
@@ -11,9 +13,28 @@ const Terminal = () => {
 
             <div className='techstack'>
                 <p>
-                    <span className='font-bold'>@abdninesix %</span>
+                    <span className='font-bold'>@abdninesix % </span>
                     show tech stack
                 </p>
+
+                <div className='label'>
+                    <p className='w-32'>Catergory</p>
+                    <p>Technologies</p>
+                </div>
+
+                <ul className='content'>
+                    {techStack.map(({ category, items }) => (
+                        <li key={category} className='flex'>
+                            <Check size={20} />
+                            <h3>{category}</h3>
+                            <ul>
+                                {items.map((item, i) => (
+                                    <li key={i}>{item}</li>
+                                ))}
+                            </ul>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </>
     )
