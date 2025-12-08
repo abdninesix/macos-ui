f85108c713ae91ae5e0f0bab682e2fbfc6150b6f
abdninesix <mohabd1995@gmail.com>
2025-12-08 20:24:32 +0500

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Welcome from './components/Welcome'
 import Dock from './components/Dock'
+import { Draggable } from 'gsap/Draggable'
+import gsap from 'gsap'
+
+gsap.registerPlugin(Draggable);
 
 const App = () => {
   return (
--- src/hoc/WindowWrapper.jsx
+++ src/hoc/WindowWrapper.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import useWindowStore from '../store/window';
+import { useRef } from 'react';
 
-const WindowWrapper = () => {
-  return (
-    <div>WindowWrapper</div>
-  )
+const WindowWrapper = (component, windowKey) => {
+
+    const Wrapped = (props) => {
+        const { focusWindow, windows } = useWindowStore();
+        const { isOpen, zIndex } = windows[windowKey]
+        const ref = useRef(null)
+
+        return <section>Hi</section>
+    }
+
+    return (
+        <div>WindowWrapper</div>
+    )
 }
 
 export default WindowWrapper
