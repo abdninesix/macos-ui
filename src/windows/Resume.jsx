fd1097a4d09f124c3bf000f0af45770c254f9373
abdninesix <mohabd1995@gmail.com>
2025-12-09 20:39:53 +0500

    Updated

--- src/windows/Resume.jsx
+++ src/windows/Resume.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import WindowsControls from '../components/WindowsControls'
 import WindowWrapper from '../hoc/WindowWrapper'
 import { Download } from 'lucide-react'
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.mjs',
+    import.meta.url,
+).toString();
 
 const Resume = () => {
     return (
@@ -11,6 +19,10 @@ const Resume = () => {
                 <h2>Resume.pdf</h2>
                 <a href="files/resume.pdf" download title='Download Resume'><Download className='icon' /></a>
             </div>
+
+            <Document file="files/resume.pdf">
+                <Page pageNumber={1} renderTextLayer renderAnnotationLayer />
+            </Document>
         </>
     )
 }
