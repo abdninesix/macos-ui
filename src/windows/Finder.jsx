6e6081cbc27bd1f2e5e85302db9f930fc7ec3513
abdninesix <mohabd1995@gmail.com>
2025-12-12 14:52:15 +0500

    Updated

--- src/windows/Finder.jsx
+++ src/windows/Finder.jsx
@@ -22,8 +22,8 @@ const Finder = () => {
                         <h3>Favorites</h3>
                         <ul>
                             {Object.values(locations).map((item) => (
-                                <li key={item.id} onClick={() => setActiveLocation(item)}>
-                                    <img src={item.icon} alt={item.name} className={`w-4 ${item.id === activeLocation.id ? "active" : "not-active"}`} />
+                                <li key={item.id} onClick={() => setActiveLocation(item)} className={item.id === activeLocation.id ? "active" : "not-active"}>
+                                    <img src={item.icon} alt={item.name} className="w-4" />
                                     <p className='text-sm font-medium truncate'>{item.name}</p>
                                 </li>
                             ))}
