80c683442d0ba976fd9cd0e4160c983b29b93168
abdninesix <mohabd1995@gmail.com>
2025-12-12 20:52:15 +0500

    Updated

--- src/windows/Finder.jsx
+++ src/windows/Finder.jsx
@@ -11,8 +11,10 @@ const Finder = () => {
     const { openWindow } = useWindowStore()
     const { activeLocation, setActiveLocation } = useLocationStore()
 
-    const openItem = () => {
-        if (item.fileType === "pdf") return openWindow("resume")
+    const openItem = (item) => {
+        if (item.fileType === "pdf") return openWindow("resume");
+        if (item.kind === "folder") return setActiveLocation(item)
+        if (["fig", "url"].includes(item.fileType) && item.href) return window.open(item.href, "_blank")
     }
 
     const renderList = (name, items) => (
