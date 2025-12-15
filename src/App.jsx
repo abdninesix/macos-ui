e9610c1c57a124013388c9167d67f11b1ed12a43
abdninesix <mohabd1995@gmail.com>
2025-12-15 20:49:36 +0500

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Resume from './windows/Resume'
 import Finder from './windows/Finder'
 import Text from './windows/Text'
 import Image from './windows/Image'
+import Contact from './windows/Contact'
 
 gsap.registerPlugin(Draggable);
 
@@ -26,6 +27,7 @@ const App = () => {
       <Finder />
       <Text />
       <Image />
+      <Contact />
     </main>
   )
 }
