d18fc4df4a663af07d4075278e81cdc4811183a1
abdninesix <mohabd1995@gmail.com>
2025-12-15 21:17:38 +0500

    Updated

--- src/store/location.js
+++ src/store/location.js
@@ -7,9 +7,10 @@ const DEFAULT_LOCATION = locations.work;
 const useLocationStore = create(
     immer((set) => ({
         activeLocation: DEFAULT_LOCATION,
-        setActiveLocation: (location = null) => set((state) => { state.activeLocation = location }),
+        setActiveLocation: (location) => set((state) => { state.activeLocation = location }),
         resetActiveLocation: () => set((state) => {
             state.activeLocation = DEFAULT_LOCATION;
+            if (location === undefined) return;
         }),
     }))
 );
