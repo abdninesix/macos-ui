00ce6a8fba69f1c2bf531bfafba2c7bf33533087
abdninesix <mohabd1995@gmail.com>
2025-12-17 12:02:37 +0500

    Updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { locations } from '../constants/data'
+
+const projects = locations.work?.children ?? []
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <section id='home'>
+            <ul>
+                {projects.map((project) => (
+                    <li key={project.id} className={`group folder ${project.windowPosition}`}>
+                        <img src="/images/folder.png" alt={project.name} />
+                        <p>{project.name}</p>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    )
 }
 
 export default Home
