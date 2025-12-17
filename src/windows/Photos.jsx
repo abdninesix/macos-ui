87333d1251b63ffa29aa107e0d7f3971685c6bb1
abdninesix <mohabd1995@gmail.com>
2025-12-17 12:58:09 +0500

    Updated

--- src/windows/Photos.jsx
+++ src/windows/Photos.jsx
@@ -4,10 +4,12 @@ import WindowWrapper from '../hoc/WindowWrapper'
 import { Mail } from 'lucide-react'
 import { Search } from 'lucide-react'
 import useWindowStore from '../store/window'
+import { photosLinks } from '../constants/data'
 
 const Photos = () => {
 
     const { openWindow } = useWindowStore();
+
     return (
         <>
             <div id='window-header'>
@@ -16,6 +18,19 @@ const Photos = () => {
                     <Mail className='icon' />
                     <Search className='icon' />
                 </div>
+            </div>
+
+            <div className='flex w-full'>
+                <div className='sidebar'>
+                    <h2>Photos</h2>
+                    <ul>{photosLinks.map(({ id, icon, title }) => (
+                        <li key={id}>
+                            <img src={icon} alt={title} />
+                            <p>{title}</p>
+                        </li>
+                    ))}</ul>
+                </div>
+                <div className='gallery'></div>
             </div>
         </>
     )
