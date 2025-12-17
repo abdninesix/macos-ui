51c313cbbdc0e348a57cea7591a6c7a583f2b4f0
abdninesix <mohabd1995@gmail.com>
2025-12-17 12:12:20 +0500

    Updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 import { locations } from '../constants/data'
 import { useGSAP } from '@gsap/react'
 import { Draggable } from 'gsap/Draggable'
+import useWindowStore from '../store/window'
+import useLocationStore from '../store/location'
 
 const projects = locations.work?.children ?? []
 
 const Home = () => {
+
+    const { setActiveLocation } = useLocationStore();
+    const { openWindow } = useWindowStore();
+
+    const handleOpenProjectFinder = (project) => {
+        setActiveLocation(project)
+        openWindow(project);
+    }
 
     useGSAP(() => {
         Draggable.create(".folder", {
@@ -17,7 +27,7 @@ const Home = () => {
         <section id='home'>
             <ul>
                 {projects.map((project) => (
-                    <li key={project.id} className={`group folder ${project.windowPosition}`}>
+                    <li key={project.id} className={`group folder ${project.windowPosition}`} onClick={() => handleOpenProjectFinder(project)}>
                         <img src="/images/folder.png" alt={project.name} />
                         <p>{project.name}</p>
                     </li>
