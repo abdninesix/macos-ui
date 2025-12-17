b95f8897fef0a02d70fd0d171714c4e3a6689354
abdninesix <mohabd1995@gmail.com>
2025-12-17 13:05:55 +0500

    Updated

--- src/windows/Photos.jsx
+++ src/windows/Photos.jsx
@@ -4,7 +4,7 @@ import WindowWrapper from '../hoc/WindowWrapper'
 import { Mail } from 'lucide-react'
 import { Search } from 'lucide-react'
 import useWindowStore from '../store/window'
-import { photosLinks } from '../constants/data'
+import { gallery, photosLinks } from '../constants/data'
 
 const Photos = () => {
 
@@ -23,12 +23,14 @@ const Photos = () => {
             <div className='flex w-full'>
                 <div className='sidebar'>
                     <h2>Photos</h2>
-                    <ul>{photosLinks.map(({ id, icon, title }) => (
-                        <li key={id}>
-                            <img src={icon} alt={title} />
-                            <p>{title}</p>
-                        </li>
-                    ))}</ul>
+                    <ul>
+                        {photosLinks.map(({ id, icon, title }) => (
+                            <li key={id}>
+                                <img src={icon} alt={title} />
+                                <p>{title}</p>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
                 <div className='gallery'>
                     {gallery.map(({ id, img }) => (
