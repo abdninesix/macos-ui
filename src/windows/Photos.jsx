b09580c00e6989d6d16a98ed72357f923f3c2545
abdninesix <mohabd1995@gmail.com>
2025-12-17 12:26:16 +0500

    Updated

--- src/windows/Photos.jsx
+++ src/windows/Photos.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import WindowsControls from '../components/WindowsControls'
+import WindowWrapper from '../hoc/WindowWrapper'
+import { Mail } from 'lucide-react'
+import { Search } from 'lucide-react'
+import useWindowStore from '../store/window'
 
 const Photos = () => {
-  return (
-    <div>Photos</div>
-  )
+
+    const { openWindow } = useWindowStore();
+    return (
+        <>
+            <div id='window-header'>
+                <WindowsControls target='photos' />
+                <div className='w-full flex justify-end items-center gap-3 text-gray-500'>
+                    <Mail className='icon' />
+                    <Search className='icon' />
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default Photos
+const PhotosWindow = WindowWrapper(Photos, 'photos')  //This Higher-Order-Compononent takes Terminal and returns a modified version of it
+
+export default PhotosWindow
